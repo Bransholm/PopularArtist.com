5ed48d0eb57ca4ab6dfc79b773b90b2cd3ab1eae
Bransholm <kenneth@bransholm.dk>
2023-09-10 22:14:54 +0200

    Show artists & simple html/css

--- Backend/server.js
+++ Backend/server.js
@@ -8,18 +8,6 @@ const port = 4000;
 app.use(express.json());
 app.use(cors());
 
-// check
-// const artists = [
-//   { id: 1, task: "Buy groceries", completed: false },
-//   { id: 2, task: "Finish homework", completed: false },
-//   { id: 3, task: "Call mom", completed: false },
-//   { id: 4, task: "Go for a run", completed: false },
-//   { id: 5, task: "Read a book", completed: false },
-//   { id: 6, task: "Write a blog post", completed: false },
-// ];
-
-//console.log(artists);
-
 //GET artists
 app.get("/artists", async (request, response) => {
   const data = await fs.readFile("data/artists.json");
--- Frontend/app.js
+++ Frontend/app.js
@@ -1,7 +1,40 @@
 "use strict"
 
+const endpoint = "http://localhost:4000";
+
 window.addEventListener("load", start);
 
 function start() {
-    console.log("Hello PopularArtists.com");
+    getArtists();
+}
+
+async function getArtists() {
+    const artists = await fetchArtists();
+    showSelectedArtists(artists);
+}
+
+async function fetchArtists() {
+    const response = await fetch(`${endpoint}/artists`);
+    const data = await response.json();
+    return data;
+}
+
+function showSelectedArtists(artists) {
+    for (const artist of artists) {
+        
+        const html = `
+            <article class="grab-artists" align="center">
+            <h3>${artist.name}</h3>
+            <img src="${artist.image}" alt ="${artist.name}">
+            <p>Birthdate: ${artist.birtgdate}</p>
+            <p>Active since: ${artist.activeSince}</p>
+            <p>Genres: ${artist.genres}</p>
+            <p>Labels: ${artist.labels}</p>
+            <p>Website: ${artist.website}</p>
+            <p>About: ${artist.shortDescription}</p>
+            </article>
+            `;
+        
+        document.querySelector("#display-artists").insertAdjacentHTML("beforeend", html);
+    }
 }
